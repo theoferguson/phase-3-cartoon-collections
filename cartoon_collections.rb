543614dbc8151ceacf7f8ae07612c18deca10d5d
Theo Ferguson <ferguson.theo@gmail.com>
2021-09-23 11:37:50 -0400

    commit tree 93d358f831e475f1477e2c2834ee136a6e67e10f

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each do |dwarf|
+    puts "#{dwarves.index(dwarf)+1}. #{dwarf}\n"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planet_calls)
+  planet_calls.map {|call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.one? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheeses)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+    cheeses.find do |cheese| 
+      cheese_types.include?(cheese)
+    end
 end
